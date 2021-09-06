fb9e4ee320e247e0dbe6db15742e3bf5a901f8d8
zsy <2773213023@qq.com>
2021-09-06 22:26:56 +0800

    code

---
+++ Rand10.java
@@ -0,0 +1,14 @@
+package cn.zhu.five;
+
+/**
+ * @ClassName Rand10
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2021/9/5 23:08
+ * @Version 1.0
+ **/
+public class Rand10 {
+    public static void main(String[] args) {
+        
+    }
+}

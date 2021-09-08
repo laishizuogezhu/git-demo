80ecbd144fb363ff4734fca3fe34625fe1fd51d9
zsy <2773213023@qq.com>
2021-09-08 10:10:12 +0800

    zhu commit

---
+++ FeiBo.java
@@ -0,0 +1,35 @@
+package cn.zhu.four;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName FeiBo
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2021/9/4 22:34
+ * @Version 1.0
+ **/
+public class FeiBo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入需要求得斐波那契数：");
+        int i = scanner.nextInt();
+        int s = fn(i);
+        System.out.println(s);
+    }
+
+    private static int fn(int n) {
+        if (n==0 || n==1){
+            return n;
+        }
+        else {
+            int a = 0,b=1;
+            for(int i = 1;i<n;i++){
+                b= a+b;
+                a= b-a;
+                b%=1000000007;
+            }
+            return b;
+        }
+    }
+}
